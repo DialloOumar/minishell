aaaecf775e90334ff4e181938e633b638beef683
Oumar <oumardiallo636@gmail.com>
2026-03-17 20:26:03 +0100

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -12,14 +12,15 @@ def main():
         user_input = input()
 
         command = user_input.split(" ")[0]
+        args = user_input.split(" ")[1]
         # Handle  the exit command
         if "exit" == command:
             return 
         if command.startswith("echo"):
             print(user_input[5:])
 
-        elif command == "type" and command in commands:
-            print(f"{command} is a shell builtin")
+        elif command == "type" and args in commands:
+            print(f"{args} is a shell builtin")
         else:
             print(f"{command}: not found")
 
