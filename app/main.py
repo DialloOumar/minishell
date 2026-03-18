fab56dcc10a8c42f7c5625173cd342e1604ad6d3
Oumar <oumardiallo636@gmail.com>
2026-03-18 21:52:29 +0100

    minor cleanup

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 import sys
 import os
 
-# path = os.environ["PATH"]
-
-# print(os.pathsep)
 # supported commands
 COMMANDS = ["exit", "echo", "type"]
 
@@ -11,11 +8,9 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
-        
         line = input()
         # Get use input
         command, *args = line.split(" ")
-
         # Handle  the exit command
         if "exit" == command:
             return 
@@ -35,12 +30,10 @@ def main():
                         is_path_found = True
                         if os.access(full_path, os.X_OK):
                             print(f"{args[0]} is {full_path}")
-
                 if not is_path_found:
                     print(f"{args[0]} not found")
         else:
             print(f"{command}: not found")
-
 
 if __name__ == "__main__":
     main()
